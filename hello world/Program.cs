c1685a0a591b51dbbff6965fa7472ae8fa78adaf
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-06 16:20:56 +0530

    initial

---
+++ hello world/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("what is your name");
+var numbers = new[] { 12.2, 10.2, 22.0, 56.9 }
+var result = numbers[0] ;
+Result = Result + numbers[1];
+Console.WriteLine(Result);
+if (args.length>0)
+    {
+    Console.WriteLine($"hello,{args[0]}!");
+}
+else
+{
+    Console.WriteLine("hi");
+}
